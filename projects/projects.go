fb906d1f268157080bd082bfbdceb571a1266530
hironori shimada <shimada.hironori@nifty.co.jp>
2024-12-10 05:26:42 +0000

    Fix JSON tags in GetWorkspaceProjectsUsersOutput struct for clarity and consistency

--- projects/projects.go
+++ projects/projects.go
@@ -16,18 +16,18 @@ type GetWorkspaceProjectsUsersInput struct {
 	Query       GetWorkspaceProjectsUsersQuery
 }
 type GetWorkspaceProjectsUsersOutput struct {
-	At                   string `json:"at"`         //	When was last modified
-	Gid                  int    `json:"gid"`        //	Group ID, legacy field
-	GroupId              int    `json:"group_id"`   //	Group ID
-	Id                   int    `json:"id"`         //	Project User ID
-	LaborCost            *int   `json:"labor_cost"` //	null
-	LaborCostLastUpdated string `json:""`           //	Date for labor cost last updated
-	Manager              bool   `json:""`           //	Whether the user is manager of the project
-	ProjectId            int    `json:""`           //	Project ID
-	Rate                 *int   `json:""`           //	null
-	RateLastUpdated      string `json:""`           //	Date for rate last updated
-	UserId               int    `json:""`           //	User ID
-	WorkspaceId          int    `json:""`           //	Workspace ID
+	At                   string `json:"at"`                      //	When was last modified
+	Gid                  int    `json:"gid"`                     //	Group ID, legacy field
+	GroupId              int    `json:"group_id"`                //	Group ID
+	Id                   int    `json:"id"`                      //	Project User ID
+	LaborCost            *int   `json:"labor_cost"`              //	null
+	LaborCostLastUpdated string `json:"labor_cost_last_updated"` //	Date for labor cost last updated
+	Manager              bool   `json:"manager"`                 //	Whether the user is manager of the project
+	ProjectId            int    `json:"prpoject_id"`             //	Project ID
+	Rate                 *int   `json:"rate"`                    //	null
+	RateLastUpdated      string `json:"rate_last_updated"`       //	Date for rate last updated
+	UserId               int    `json:"user_id"`                 //	User ID
+	WorkspaceId          int    `json:"workspace_id"`            //	Workspace ID
 }
 
 func (c Client) GetWorkspaceProjectsUsers(workspaceID int) string {
